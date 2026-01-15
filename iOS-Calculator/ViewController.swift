3476d6f9a8139bf838b9f007a2e18aa37a792d77
Candy Montero <candymdeoleo@gmail.com>
2026-01-15 21:00:31 +0100

    Initial Commit

---
+++ iOS-Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Calculator
+//
+//  Created by Candy 💗 on 15/1/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS-CalculatorTests/iOS_CalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_CalculatorTests.swift
+//  iOS-CalculatorTests
+//
+//  Created by Candy 💗 on 15/1/26.
+//
+
+import Testing
+@testable import iOS_Calculator
+
+struct iOS_CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
